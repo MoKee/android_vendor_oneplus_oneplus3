11141031adc397875a8c992de3c0d89ae0a659e5
dianlujitao <dianlujitao@gmail.com>
2016-07-29 21:23:53 +0900

    op3: Ship radio images
    Change-Id: I1894d80893ca53fbe65b5e6ab34eb6c2524693ce

--- Android.mk
+++ Android.mk
@@ -268,4 +268,13 @@ endif
 
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
+
+ifeq ($(LOCAL_PATH)/radio, $(wildcard $(LOCAL_PATH)/radio))
+
+RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*)
+$(foreach f, $(notdir $(RADIO_FILES)), \
+    $(call add-radio-file,radio/$(f)))
+$(call add-radio-file,../../../device/oneplus/oneplus3/radio/filesmap)
+endif
+
 endif
